8ff7a0d7fb06236c62910fe99c797eee56dc1eba
jxatgithub <xjingjin@gmail.com>
2016-01-22 11:59:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## This program will allow users to input a square matrix and 
+## calculate the inverse of it. Then it will be cached.
+## The function cacheSove will first decide if it the inverse of a 
+## matrix already exists in the cache. If it does, the function 
+## will output the exiting value; if not, the function will 
+## calculate and output it
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-123
+    
 }
 
 
