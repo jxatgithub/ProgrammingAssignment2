dcc36e3d3b028cdebe45ee2ecdbccf89c7b7a8dc
jxatgithub <xjingjin@gmail.com>
2016-01-22 12:23:23 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat_inv <- NULL
+  if (ncol(x) == nrow(x)){
+    mat_inv <<- solve(x)
+  }
+  else {
+    message("Not a square matrix!")
+    return(x)
+  }
+ 
 }
 
 
 ## Write a short comment describing this function
+## The cacheSolve function will first check if the inverse of the input matrix exists,
+## If not, it will call makeCheMatrix to calculate the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if (!is.null(mat_inv)){
+    message("getting cached data...")
+    return(mat_inv)
+  }
+  else{
+    makeCacheMatrix
+  }
+  
+        
+        
 }
